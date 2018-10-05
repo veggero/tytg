6ad84f252956c92de7045d12a1dde1cbe664ae16
veggero <niccolo.venerandi@gmail.com>
2018-10-05 12:56:40 +0200

    Changed instructions

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
 setup(name='tytg',
-      version='0.2',
+      version='0.2.1',
       description='Telegram bot making without any line of code.',
       url='http://github.com/veggero/tytg',
       author='veggero',
       author_email='niccolo@venerandi.com',
       license='unlicenced',
       packages=['tytg'],
+      long_description=open('README.md', 'r').read(),
+	  long_description_content_type="text/markdown",
       install_requires=[
           'python-telegram-bot',
       ],
+		entry_points='''
+			[console_scripts]
+			tytg=tytg:tytg
+		''',
       zip_safe=False)

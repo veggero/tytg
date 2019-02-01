83be74abe5e419aa15d700a4faa615d211fc87a6
veggero <niccolo@venerandi.com>
2019-02-01 17:19:23 +0100

    Finished what I think I wanted to do with the Mailbox class in a quite weird way many people will have a hard time understanding

--- tytg/Mailbox.py
+++ tytg/Mailbox.py
@@ -7,13 +7,12 @@ class MailBox:
 	sending files.
 	"""
 	
-	supported_exts = ('.txt', '.html', '.jpg', '.png', '.bmp', '.mp4',
-		              '.mp3', '.gif', '.tgfile', '.py')
-	
 	ext_to_type = {'.txt': 'text', '.html': 'html', '.jpg': 'photo',
 				   '.png': 'photo', '.bmp': 'photo', '.mp4': 'video',
 				   '.mp3': 'voice', '.gif': 'document',
 				   '.py': 'python', '.tgfile': 'telegramfile'}
+	
+	supported_exts = *ext_to_type,
 	
 	def __init__(self, user, bot):
 		"""
@@ -47,12 +46,12 @@ class MailBox:
 		# in a file named after the file_id itself.
 		# Add the file to the message text.
 		if f_id:
-			with open(f'{f_id}.tgfile', 'w') as file:
+			with open(f'.{f_id}.tgfile', 'w') as file:
 				file.write(f'{ext}@{f_id}')
-			text += f'\n{f_id}.tgfile'
-		
+			text += f'\n.{f_id}.tgfile'
+
 		return text
-		
+
 	def send(self, message):
 		"""
 		This method takes a message and checks for
@@ -72,12 +71,35 @@ class MailBox:
 			
 			# Open the file in this line and send it
 			ext = os.path.splitext(line)[1]
-			self.send_file(ext_to_type[ext], open(line, 'rb'))
+			self.send_file(self.ext_to_type[ext], open(line, 'rb'))
 			
 		self.send_file('text', message.strip())
 		
-	def send_file(self, extension, document):
+	def send_file(self, extension, directory, f_id=None):
 		"""
 		Given a file and an extension (photo, voice, etc.)
 		send it to telegram
 		"""
+		if extension == 'python':
+			return self.send(get_docstring(directory))
+		if extension == 'telegramfile':
+			tgcontent = open(directory, 'r').read()
+			extension, div, f_id = tgcontent.partition('@')
+		ext_to_fun = {'text': self.bot.send_message,
+					  'html': self.bot.send_message,
+					  'photo': self.bot.send_photo,
+					  'video': self.bot.send_video,
+					  'voice': self.bot.send_voice,
+					  'document': self.bot.send_document}
+		document = f_id or (open(document, 'r').read() if extension 
+			in ('text', 'html') else photo = open(document, 'rb'))
+		kw = ('html',) * (extension == 'html') 
+		ext_to_fun[extension](self.user, document, **kw)
+
+
+def get_docstring(source):
+	source = open(source, 'r').read()
+	for node in ast.walk(ast.parse(source)):
+		if isinstance(node, ast.Module):
+			docstring = ast.get_docstring(node)
+			return docstring
